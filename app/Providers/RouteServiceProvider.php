a482801199e9def05bcd3481a20f3b4f94428ef9
Mohammad Akbari <mohammad.akbari@outlook.com>
2024-01-01 22:44:07 +0330

    Add service providers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->group(base_path('routes/sibcheh.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
