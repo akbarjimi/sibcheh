ccb7fb5ce5709b8702a944d29bb84ae8c09ec1e8
Mohammad Akbari <mohammad.akbari@outlook.com>
2024-01-01 22:37:19 +0330

    Add migrations

--- database/migrations/2023_12_29_170611_create_locations_table.php
+++ database/migrations/2023_12_29_170611_create_locations_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('passport')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('locations');
     }
 };
---
+++ database/migrations/2023_12_29_170937_create_flights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Location;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Location::class, 'from_location_id');
+            $table->foreignIdFor(Location::class, 'to_location_id');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
